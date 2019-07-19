4fd308cfc533b1ff13c12a51a3c54d569b68cefe
Craig Robson <craig.robson1@ncl.ac.uk>
2019-07-19 15:28:52 +0100

    fixed errors in making calls dynamic

--- spatial_join_areas.py
+++ spatial_join_areas.py
@@ -14,31 +14,33 @@ def create_database_connection(db_params):
     return connection
 
 
-def main(connection=None, connection_parameters=None, dataset='', join_multiple_areas=True, areas_to_join='', join_data_is_areas=True, fields_to_join=[], dataset_id_field='gid', join_areas_id_field='gid', dataset_geom_field='geom', join_areas_geom_field='geom'):
+def main(database_connection=None, connection_parameters=None, dataset='', join_multiple_areas=True, areas_to_join='', join_data_is_areas=True, fields_to_join=[], dataset_id_field='gid', join_areas_id_field='gid', dataset_geom_field='geom', join_areas_geom_field='geom'):
     """
     Run the spatial join over two tables existing in a postgres database.
 
     fields_to_join:
      - [{name:xxx, type:xxx},{name:yyy, type:yyy}]
     """
-    if connection is None:
+    if database_connection is None:
         if connection_parameters is None:
             # return an error to the user
             return
-        db_connection = create_database_connection(connection_parameters)
+        database_connection = create_database_connection(connection_parameters)
 
     # create cursor to access database
-    cursor = db_connection.cursor()
+    cursor = database_connection.cursor()
 
     temp_table = '___temp'
 
     if join_multiple_areas:
         # run join
         if join_data_is_areas:
-            cursor.execute(sql.SQL('SELECT b.%s, ARRAY_AGG(st_area(st_intersection(b.%s, t.geom)) / st_area(b.%s)) as coverage, ARRAY_AGG(t.geo_code) as lads, ARRAY_AGG(t.geo_code_gor) as gors INTO {} FROM ftables.{} t, {} b WHERE st_intersects(b.%s, t.%s) ORDER BY g.gid;').format(sql.Identifier(temp_table), sql.Identifier(areas_to_join), sql.Identifier(dataset)), [dataset_id_field, join_areas_geom_field, dataset_geom_field, dataset_geom_field, join_areas_geom_field])
+            cursor.execute(sql.SQL('SELECT b.{0} as gid, ARRAY_AGG(st_area(st_intersection(b.{1}, t.{2})) / st_area(b.{3})) as coverage, ARRAY_AGG(t.geo_code) as lads, ARRAY_AGG(t.geo_code_gor) as gors INTO {9} FROM ftables.{4} t, {5} b WHERE st_intersects(b.{6}, t.{7}) GROUP BY b.{8};').format(sql.Identifier(dataset_id_field), sql.Identifier(dataset_geom_field), sql.Identifier(join_areas_geom_field), sql.Identifier(dataset_geom_field), sql.Identifier(areas_to_join), sql.Identifier(dataset), sql.Identifier(dataset_geom_field), sql.Identifier(join_areas_geom_field), sql.Identifier(temp_table)), [])
         else:
-            cursor.execute(sql.SQL('SELECT b.gid, ARRAY_AGG(t.geo_code) as lads, ARRAY_AGG(t.geo_code_gor) as gors INTO ___temp FROM ftables.{} t, {} b WHERE st_intersects(b.geom_, t.geom) GROUP BY b.gid;').format(sql.Identifier(areas_to_join), sql.Identifier(dataset)))
-
+            #print('Running line & point join')
+            #cursor.execute(sql.SQL('SELECT b.{0} as gid INTO {2} FROM {1} b;').format(sql.Identifier('gid'), sql.Identifier(dataset), sql.Identifier('_temp')))
+            cursor.execute(sql.SQL('SELECT b.{0} as gid, ARRAY_AGG(t.geo_code) as lads, ARRAY_AGG(t.geo_code_gor) as gors INTO {5} FROM ftables.{1} t, {2} b WHERE st_intersects(b.{3}, t.{4}) GROUP BY b.{6};').format(sql.Identifier(dataset_id_field), sql.Identifier(areas_to_join), sql.Identifier(dataset), sql.Identifier(dataset_geom_field), sql.Identifier(join_areas_geom_field), sql.Identifier(temp_table), sql.Identifier(dataset_id_field)))
+        #print('Generated temp table')
         # create fields in dataset to join areas to
         #for field in fields_to_join:
         cursor.execute(sql.SQL('ALTER TABLE {} ADD IF NOT EXISTS lads character varying[];').format(sql.Identifier(dataset)))
@@ -49,19 +51,20 @@ def main(connection=None, connection_parameters=None, dataset='', join_multiple_
             cursor.execute(sql.SQL('ALTER TABLE {} ADD IF NOT EXISTS lad_coverage float[];').format(sql.Identifier(dataset)))
 
         # create index on the ids to speed up the update
-        cursor.execute(sql.SQL('CREATE INDEX %s ON {} USING btree(gid);').format(sql.Identifier(temp_table)), [temp_table+'_gid_idx'])
-        cursor.execute(sql.SQL('CREATE INDEX IF NOT EXISTS %s ON {} USING btree(gid);').format(sql.Identifier(dataset)), [dataset+'_gid_idx'])
+        #temp = temp_table+'_gid_idx'
+        cursor.execute(sql.SQL('CREATE INDEX {0} ON {1} USING btree (gid);').format(sql.Identifier(temp_table+'_gid_idx'), sql.Identifier(temp_table)), [])
+        cursor.execute(sql.SQL('CREATE INDEX IF NOT EXISTS {0} ON {1} USING btree (gid);').format(sql.Identifier(dataset+'_gid_idx'), sql.Identifier(dataset)), [])
 
         # run the update
-        cursor.execute(sql.SQL('UPDATE {} as a SET lads = b.lads, gors = b.gors, lad_coverage = b.coverage FROM {} b WHERE a.gid = b.gid;').format(sql.Identifier(dataset), sql.Identifier(temp_table)))
+        cursor.execute(sql.SQL('UPDATE {0} as a SET lads = b.lads, gors = b.gors FROM {1} b WHERE a.{2} = b.gid;').format(sql.Identifier(dataset), sql.Identifier(temp_table), sql.Identifier(dataset_id_field)))
 
         # remote the temp tables
-        cursor.execute(sql.SQL('DROP INDEX %s;').format(sql.Identifier()), [temp_table+'_gid_idx'])
-        cursor.execute(sql.SQL('DROP TABLE {};').format(sql.Identifier(temp_table)))
+        cursor.execute(sql.SQL('DROP INDEX IF EXISTS {0};').format(sql.Identifier(temp_table+'_gid_idx')), [])
+        cursor.execute(sql.SQL('DROP TABLE IF EXISTS {0};').format(sql.Identifier(temp_table)))
 
         # create index on new fields which are now populated
-        cursor.execute(sql.SQL('CREATE INDEX IF NOT EXISTS %s ON {} USING gin(lads);').format(sql.Identifier(dataset)), [dataset+'_lads_idx'])
-        cursor.execute(sql.SQL('CREATE INDEX IF NOT EXISTS %s ON {} USING gin(gors);').format(sql.Identifier(dataset)), [dataset+'_gor_idx'])
+        cursor.execute(sql.SQL('CREATE INDEX IF NOT EXISTS {0} ON {1} USING gin(lads);').format(sql.Identifier(dataset+'_lads_idx'), sql.Identifier(dataset)), [])
+        cursor.execute(sql.SQL('CREATE INDEX IF NOT EXISTS {0} ON {1} USING gin(gors);').format(sql.Identifier(dataset+'_gor_idx'), sql.Identifier(dataset)), [])
 
     else:
         # if join result is to find a single area rather than multiple
@@ -69,4 +72,6 @@ def main(connection=None, connection_parameters=None, dataset='', join_multiple_
         # there should be some pre-processing in here before the spatial join runs
 
         # run spatial join
-        cursor.execute(sql.SQL('UPDATE {} a SET lad = b.lad_code, gor = b.gor_code FROM ftables.{} b WHERE ST_Contains(b.geom, a.centroid);').format(sql.Identifier(dataset), sql.Identifier(areas_to_join)))
+        cursor.execute(sql.SQL('UPDATE {0} a SET lad = b.lad_code, gor = b.gor_code FROM ftables.{1} b WHERE ST_Contains(b.{2}, a.centroid);').format(sql.Identifier(dataset), sql.Identifier(areas_to_join), sql.Identifier(dataset_geom_field)))
+
+    return True
